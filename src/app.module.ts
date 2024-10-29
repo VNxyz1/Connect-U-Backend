e50b433b2dd56194e95add19485eb5fdb606410d
VNxyz1 <vincenzo_n@rocketmail.com>
2024-10-29 21:09:44 +0100

    set up: - typeorm - config with .env files - updated vulnerable typeorm version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { databaseConfig, databaseConfigForFeature } from './database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local','.env.development', '.env'],
+    }),
+    databaseConfig(),
+    databaseConfigForFeature(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database.config.ts
@@ -0,0 +1,24 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-class-or-schema.type';
+
+/**
+ * All entities of the database.
+ */
+export const typeOrmEntities: EntityClassOrSchema[] = [];
+
+export const databaseConfig = () =>
+  TypeOrmModule.forRoot({
+    // @ts-ignore
+    type: process.env.DATABASE_TYPE || undefined,
+    host: process.env.DATABASE_HOST || undefined,
+    port: Number(process.env.DATABASE_PORT) || undefined,
+    username: process.env.DATABASE_USERNAME || undefined,
+    password: process.env.DATABASE_PASSWORD || undefined,
+    database: process.env.DATABASE_DATABASENAME || undefined,
+    entities: typeOrmEntities,
+    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE) || false,
+  });
+
+
+export const databaseConfigForFeature = () =>
+  TypeOrmModule.forFeature(typeOrmEntities);
