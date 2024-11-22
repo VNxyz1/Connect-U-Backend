c981743e80102d523a83e0d48618321efab0231b
VNxyz1 <vincenzo_n@rocketmail.com>
2024-11-22 15:10:59 +0100

    wrote tests for the auth controller

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -16,6 +16,8 @@ import { JWTConstants } from './constants';
 import { AuthGuard } from './auth.guard';
 import { UserService } from '../user/user.service';
 import { mockProviders } from '../../test/mock-services';
+import { CheckLoginRes } from './DTO/CheckLoginRes';
+import { OkDTO } from '../serverDTO/OkDTO';
 describe('AuthController', () => {
   let app: INestApplication;
   let agent: Agent;
@@ -68,37 +70,104 @@ describe('AuthController', () => {
     await app.init();
   });
 
-  it('/POST login', async () => {
-    mockAuthService.signIn = jest.fn().mockResolvedValue({
-      access_token: 'valid_access_token',
-      refresh_token: 'valid_refresh_token',
-    });
-    return agent
-      .post('/auth/login')
-      .send(mockLogin)
-      .expect('Content-Type', /json/)
-      .expect(HttpStatus.OK)
-      .expect({ access_token: 'valid_access_token' });
-  });
-
-  it('should return 401 Unauthorized if login credentials are incorrect', async () => {
-    mockAuthService.signIn = jest
-      .fn()
-      .mockRejectedValue(new UnauthorizedException('Invalid credentials'));
-
-    return agent
-      .post('/auth/login')
-      .send({ email: 'wrong@gmail.com', password: 'wrongpassword' })
-      .expect('Content-Type', /json/)
-      .expect(HttpStatus.UNAUTHORIZED)
-      .expect({
-        statusCode: HttpStatus.UNAUTHORIZED,
-        message: 'Invalid credentials',
-        error: 'Unauthorized',
+  describe('/POST login', () => {
+    it('/POST login', async () => {
+      mockAuthService.signIn = jest.fn().mockResolvedValue({
+        access_token: 'valid_access_token',
+        refresh_token: 'valid_refresh_token',
       });
+      return agent
+        .post('/auth/login')
+        .send(mockLogin)
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.OK)
+        .expect({ access_token: 'valid_access_token' });
+    });
+
+    it('should return 401 Unauthorized if login credentials are incorrect', async () => {
+      mockAuthService.signIn = jest
+        .fn()
+        .mockRejectedValue(new UnauthorizedException('Invalid credentials'));
+
+      return agent
+        .post('/auth/login')
+        .send({ email: 'wrong@gmail.com', password: 'wrongpassword' })
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.UNAUTHORIZED)
+        .expect({
+          statusCode: HttpStatus.UNAUTHORIZED,
+          message: 'Invalid credentials',
+          error: 'Unauthorized',
+        });
+    });
   });
 
-  // Weitere Tests hier...
+  describe('/GET refresh', () => {
+    it('/GET refresh', async () => {
+      mockAuthService.refreshAccessToken = jest.fn().mockResolvedValue({
+        access_token: 'valid_access_token',
+      });
+
+      return agent
+        .get('/auth/refresh')
+        .set('Cookie', ['refresh_token=valid_request_token'])
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.OK)
+        .expect({ access_token: 'valid_access_token' });
+    });
+
+    it('should return bad request when no cookie is set', async () => {
+      return agent.get('/auth/refresh').expect(HttpStatus.BAD_REQUEST);
+    });
+  });
+
+  describe('/GET check-login', () => {
+    it('/GET check-login', async () => {
+      const validRes = new CheckLoginRes(true);
+
+      return agent
+        .get('/auth/check-login')
+        .set('Cookie', ['refresh_token=valid_request_token'])
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.OK)
+        .expect(JSON.stringify(validRes));
+    });
+
+    it('should return false when the user has no active refresh-token as cookie', async () => {
+      const validRes = new CheckLoginRes(false);
+
+      return agent
+        .get('/auth/check-login')
+        .expect(HttpStatus.OK)
+        .expect(JSON.stringify(validRes));
+    });
+  });
+
+  describe('/DELETE logout', () => {
+    it('/DELETE logout', async () => {
+      mockAuthService.refreshAccessToken = jest.fn().mockResolvedValue({
+        access_token: 'valid_access_token',
+      });
+
+      const validRes = new OkDTO(true, 'User is logged out.');
+
+      return agent
+        .delete('/auth/logout')
+        .set('Cookie', ['refresh_token=valid_request_token'])
+        .expect('Content-Type', /json/)
+        .expect(HttpStatus.OK)
+        .expect(JSON.stringify(validRes));
+    });
+
+    it('should return okay, even when no cookie is set', async () => {
+      const validRes = new OkDTO(true, 'User is logged out.');
+
+      return agent
+        .delete('/auth/logout')
+        .expect(HttpStatus.OK)
+        .expect(JSON.stringify(validRes));
+    });
+  });
 
   afterEach(async () => {
     await app.close();
