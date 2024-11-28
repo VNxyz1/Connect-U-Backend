ea99861cfcce576703104a2feb64dcc0b74f02cc
tinahsr <tinahaser@gmail.com>
2024-11-28 17:17:54 +0100

    feat: started with tests

--- src/request/request.service.spec.ts
+++ src/request/request.service.spec.ts
@@ -7,6 +7,8 @@ import { UserDB } from '../database/UserDB';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { NotFoundException } from '@nestjs/common';
+import { GenderEnum } from '../database/enums/GenderEnum';
+import { StatusEnum } from '../database/enums/StatusEnum';
 
 describe('RequestService', () => {
   let service: RequestService;
@@ -122,8 +124,241 @@ describe('RequestService', () => {
 
     expect(requestRepository.save).toHaveBeenCalledWith(mockRequest);
   });
+
+  it('should retrieve all requests made by a specific user', async () => {
+    const mockUser = mockUserList[0];
+    const mockRequest = { id: 1 } as RequestDB;
+
+    userRepository.findOne.mockResolvedValue(mockUserList[0]);
+    requestRepository.findOne.mockResolvedValue(null);
+
+    mockUser.requests.push(mockRequest);
+
+    const result = await service.getRequestsByUser('1');
+    expect(result).toEqual([mockRequest]);
+  });
+
+  it('should retrieve all non-denied requests for an event', async () => {
+
+    eventRepository.findOne.mockResolvedValue(mockEventList[0]);
+
+    const result = await service.getRequestsForEvent('1', '2');
+    expect(result).toEqual(mockEventList[0].requests);
+  });
+
+  it('should accept a join request and add the user to participants', async () => {
+    const mockEvent = {
+      id: 'event123',
+      host: { id: 'host123' },
+      participants: [],
+      participantsNumber: 5,
+    } as EventDB;
+    const mockRequest = {
+      id: 1,
+      user: { id: 'user123' },
+      event: mockEvent,
+      denied: false,
+    } as RequestDB;
+
+    eventRepository.findOne.mockResolvedValue(mockEvent);
+    requestRepository.findOne.mockResolvedValue(mockRequest);
+    requestRepository.remove.mockResolvedValue(undefined);
+
+    await service.acceptJoinRequest(1, 'host123');
+
+    expect(mockEvent.participants).toContain(mockRequest.user);
+    expect(requestRepository.remove).toHaveBeenCalledWith(mockRequest);
+  });
+
+  it('should deny a join request', async () => {
+    const mockEvent = { host: { id: 'host123' } } as EventDB;
+    const mockRequest = { id: 1, event: mockEvent, denied: false } as RequestDB;
+
+    eventRepository.findOne.mockResolvedValue(mockEvent);
+    requestRepository.findOne.mockResolvedValue(mockRequest);
+    requestRepository.save.mockResolvedValue({ ...mockRequest, denied: true });
+
+    await service.denyRequest(1, 'host123');
+
+    expect(requestRepository.save).toHaveBeenCalledWith({ ...mockRequest, denied: true });
+  });
+
+  it('should delete a join request', async () => {
+    const mockRequest = { id: 1, user: { id: 'user123' } } as RequestDB;
+
+    requestRepository.findOne.mockResolvedValue(mockRequest);
+    requestRepository.remove.mockResolvedValue(undefined);
+
+    await service.deleteJoinRequest(1, 'user123');
+    expect(requestRepository.remove).toHaveBeenCalledWith(mockRequest);
+  });
 });
 
 export const mockRequestService = {
   postJoinRequest: jest.fn().mockResolvedValue(new RequestDB()),
 };
+
+
+const mockUserList: UserDB[] = [
+  {
+    id: '1',
+    email: 'john.doe@example.com',
+    username: 'johndoe',
+    password: 'hashedpassword123',
+    firstName: 'John',
+    lastName: 'Doe',
+    birthday: '1990-01-15',
+    phoneNumber: '+1234567890',
+    profilePicture: 'profile1.png',
+    pronouns: 'he/him',
+    profileText: 'I love programming and outdoor adventures.',
+    streetNumber: '123',
+    street: 'Main St',
+    zipCode: '12345',
+    city: 'Anytown',
+    isVerified: true,
+    gender: GenderEnum.Male,
+    hostedEvents: [],
+    requests: [],
+    participatedEvents: [],
+    favoritedEvents: [],
+    memories: [],
+    friends: Promise.resolve([]),
+    friendOf: Promise.resolve([]),
+    listEntries: [],
+    achievements: Promise.resolve([]),
+    surveyEntries: Promise.resolve([]),
+    messages: [],
+    reactions: [],
+    tags: [],
+    unreadMessages: [],
+  },
+  {
+    id: '2',
+    email: 'jane.smith@example.com',
+    username: 'janesmith',
+    password: 'hashedpassword456',
+    firstName: 'Jane',
+    lastName: 'Smith',
+    birthday: '1985-06-25',
+    phoneNumber: '+1987654321',
+    profilePicture: 'profile2.png',
+    pronouns: 'she/her',
+    profileText: 'Passionate about design and photography.',
+    streetNumber: '456',
+    street: 'Second St',
+    zipCode: '54321',
+    city: 'Othertown',
+    isVerified: false,
+    gender: GenderEnum.Female,
+    hostedEvents: [],
+    requests: [],
+    participatedEvents: [],
+    favoritedEvents: [],
+    memories: [],
+    friends: Promise.resolve([]),
+    friendOf: Promise.resolve([]),
+    listEntries: [],
+    achievements: Promise.resolve([]),
+    surveyEntries: Promise.resolve([]),
+    messages: [],
+    reactions: [],
+    tags: [],
+    unreadMessages: [],
+  },
+  {
+    id: '3',
+    email: 'alex.jones@example.com',
+    username: 'alexjones',
+    password: 'hashedpassword789',
+    firstName: 'Alex',
+    lastName: 'Jones',
+    birthday: '2000-10-10',
+    phoneNumber: '+1122334455',
+    profilePicture: 'profile3.png',
+    pronouns: 'they/them',
+    profileText: 'Avid reader and aspiring writer.',
+    streetNumber: '789',
+    street: 'Third St',
+    zipCode: '67890',
+    city: 'Somecity',
+    isVerified: true,
+    gender: GenderEnum.Diverse,
+    hostedEvents: [],
+    requests: [],
+    participatedEvents: [],
+    favoritedEvents: [],
+    memories: [],
+    friends: Promise.resolve([]),
+    friendOf: Promise.resolve([]),
+    listEntries: [],
+    achievements: Promise.resolve([]),
+    surveyEntries: Promise.resolve([]),
+    messages: [],
+    reactions: [],
+    tags: [],
+    unreadMessages: [],
+  },
+];
+
+
+const mockEventList: EventDB[] = [
+  {
+    id: '1',
+    title: 'Tech Conference 2024',
+    description: 'A conference for tech enthusiasts.',
+    dateAndTime: '2024-12-01T10:00:00',
+    categories: [],
+    host: mockUserList[1],
+    type: EventtypeEnum.halfPrivate,
+    isOnline: false,
+    showAddress: true,
+    streetNumber: '456',
+    street: 'Tech Ave',
+    zipCode: '67890',
+    city: 'Tech City',
+    participantsNumber: 100,
+    preferredGenders: [],
+    status: StatusEnum.upcoming,
+    picture: '',
+    startAge: 0,
+    endAge: 0,
+    participants: [],
+    requests: [],
+    surveys: [],
+    lists: [],
+    favorited: [],
+    memories: [],
+    tags: [],
+    messages: [],
+  },
+  {
+    id: '2',
+    title: 'Game Jam 2024',
+    description: 'Game Jam to create awesome new games!',
+    dateAndTime: '2024-12-01T10:00:00',
+    categories: [],
+    host: mockUserList[1],
+    type: EventtypeEnum.halfPrivate,
+    isOnline: true,
+    showAddress: true,
+    streetNumber: '',
+    street: '',
+    zipCode: '',
+    city: '',
+    participantsNumber: 23,
+    preferredGenders: [],
+    status: StatusEnum.upcoming,
+    picture: '',
+    startAge: 0,
+    endAge: 0,
+    participants: [],
+    requests: [],
+    lists: [],
+    favorited: [],
+    surveys: [],
+    memories: [],
+    tags: [],
+    messages: [],
+  },
+];
