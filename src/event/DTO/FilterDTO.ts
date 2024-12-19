894985abe7352e465541380a35ca4c58fdc35ffc
tinahsr <tinahaser@gmail.com>
2024-12-19 14:45:17 +0100

    feature: updated dto

--- src/event/DTO/FilterDTO.ts
+++ src/event/DTO/FilterDTO.ts
@@ -1,15 +1,76 @@
 import {
-  IsDateString,
   IsOptional,
+  IsBoolean,
+  IsString,
+  IsNumber,
+  Min,
+  Max
 } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
-export class FilterDTO {
+export class FilterEventDTO {
   @ApiPropertyOptional({
-    description: 'the date of the drive',
-    example: '2024-06-13T18:21:15.068Z',
+    description: 'Search for events by title',
+    example: 'Music Festival',
   })
   @IsOptional()
-  @IsDateString()
-  date?: Date;
+  @IsString()
+  title?: string;
+
+  @ApiPropertyOptional({
+    description: 'The minimum age allowed for the event',
+    example: 18,
+  })
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  minAge?: number;
+
+  @ApiPropertyOptional({
+    description: 'The maximum age allowed for the event',
+    example: 35,
+  })
+  @IsOptional()
+  @IsNumber()
+  @Max(120)
+  maxAge?: number;
+
+  @ApiPropertyOptional({
+    description: 'Allowed genders for the event (e.g., male, female, diverse',
+    example: ['male', 'female'],
+  })
+  @IsOptional()
+  genders?: string[];
+
+  @ApiPropertyOptional({
+    description: 'Whether the event is public',
+    example: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isPublic?: boolean;
+
+  @ApiPropertyOptional({
+    description: 'Whether the event is half public',
+    example: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isHalfPublic?: boolean;
+
+  @ApiPropertyOptional({
+    description: 'Whether the event is online',
+    example: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isOnline?: boolean;
+
+  @ApiPropertyOptional({
+    description: 'Whether the event is in a physical location',
+    example: false,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isInPlace?: boolean;
 }
