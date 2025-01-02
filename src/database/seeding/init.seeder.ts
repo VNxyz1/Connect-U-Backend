013fa393989b013b9c97ac36ba1c45983ee0a7f7
VNxyz1 <vincenzo_n@rocketmail.com>
2025-01-02 15:53:03 +0100

    it is now possible to set a desired user and event count

--- src/database/seeding/init.seeder.ts
+++ src/database/seeding/init.seeder.ts
@@ -7,6 +7,7 @@ import { eventFactory } from './event.factory';
 import { CategoryDB } from '../CategoryDB';
 import { TagService } from '../../tag/tag.service';
 import { fakerDE as faker } from '@faker-js/faker';
+import * as process from 'node:process';
 
 export class InitSeeder {
   constructor(
@@ -21,30 +22,44 @@ export class InitSeeder {
 
   async onApplicationBootstrap() {
     const populate = Boolean(process.env.POPULATE_DB) || false;
-
     if (populate) {
-      await this.initUsers();
-      const users = await this.userRepository.find({});
-      const categories = await this.categoryRepository.find({});
+      const userCount = await this.userRepository.count();
+      const desiredUsers = Number(process.env.DESIRED_USERS) || 50;
+      const addUsers = userCount < desiredUsers;
 
-      await this.initEvents(users, categories);
+      if (addUsers) {
+        await this.initUsers(desiredUsers - userCount);
+      }
+
+      const eventCount = await this.eventRepository.count();
+      const desiredEvents = Number(process.env.DESIRED_EVENTS) || 20;
+      const addEvents = eventCount < desiredEvents;
+      if (addEvents) {
+        const users = await this.userRepository.find({});
+        const categories = await this.categoryRepository.find({});
+        await this.initEvents(users, categories, desiredEvents - eventCount);
+      }
     }
   }
 
-  private async initUsers() {
+  private async initUsers(count: number) {
     const users = [];
 
-    for (let i = 0; i < 150; i++) {
+    for (let i = 0; i < count; i++) {
       users.push(userFactory());
     }
 
     return this.userRepository.save(users);
   }
 
-  private async initEvents(userList: UserDB[], categorys: CategoryDB[]) {
+  private async initEvents(
+    userList: UserDB[],
+    categorys: CategoryDB[],
+    count: number,
+  ) {
     const events = [];
 
-    for (let i = 0; i < 80; i++) {
+    for (let i = 0; i < count; i++) {
       const tags = await this.tagService.findOrCreateTags(
         faker.word.words({ count: { min: 2, max: 5 } }).split(' '),
       );
