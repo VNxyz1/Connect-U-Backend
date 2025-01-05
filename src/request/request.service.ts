5b37c9a41ea1ca063db6309b5ac9fe044d5b4cdc
tinahsr <tinahaser@gmail.com>
2025-01-05 14:37:11 +0100

    fix: checks if users are friends

--- src/request/request.service.ts
+++ src/request/request.service.ts
@@ -10,10 +10,12 @@ import { RequestDB } from '../database/RequestDB';
 import { UserDB } from '../database/UserDB';
 import { EventDB } from '../database/EventDB';
 import { EventtypeEnum } from '../database/enums/EventtypeEnum';
+import { FriendService } from '../friend/friend.service';
 
 @Injectable()
 export class RequestService {
   constructor(
+    private readonly friendService: FriendService,
     @InjectRepository(RequestDB)
     private readonly requestRepository: Repository<RequestDB>,
     @InjectRepository(EventDB)
@@ -274,6 +276,10 @@ export class RequestService {
 
     if (existingInvite) {
       throw new BadRequestException('User is already invited to this event');
+    }
+
+    if ((await this.friendService.areUsersFriends(hostId, user.id)) === false) {
+      throw new BadRequestException('You can only invite friends');
     }
 
     const invite = this.requestRepository.create();
